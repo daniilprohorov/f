9257bc3ce47a2a2b12742d946068458fc119300f
daniilprohorov <daniilprohorov@gmail.com>
2019-09-12 02:29:05 +0300

    2

--- f.py
+++ f.py
@@ -1,37 +1,111 @@
-
 import curses
 import os
 import subprocess as s
 
-mmask = curses.ALL_MOUSE_EVENTS
-screen = curses.initscr()
-curses.curs_set(0)
-screen.keypad(1)
-curses.mousemask(mmask)
-curses.noecho()
+def init():
+    mmask = curses.ALL_MOUSE_EVENTS
+    screen = curses.initscr()
+    curses.curs_set(0)
+    screen.keypad(1)
+    curses.mousemask(mmask)
+    curses.noecho()
+    return (curses, screen)
+
+
+def exit(curses):
+    curses.endwin()
+
+def folder(name):
+    w = 16
+    h = 7
+    f1 ="  ╔══════════╗        "    
+    f2 =" ║            ╚════╗  "
+    fs =" ║ " # 7 # # # # # #                
+    f3 ="  ╚════════════════╝  "
+    lst = [f1,f2]+[fs+('{:^'+str(w)+'}').format(name[w*i:w*(i+1)])+fs for i in range(h-1)]+[f3] 
+    return lst
+    
+
+kek = [" 1*** ", " 2*** ", " 3*** ", " 4*** "]
+
+def createGrid(folderLst, screen):
+    _, w = screen.getmaxyx() 
+    h = 16
+    oneFolder = folderLst[0]
+    nWidth = max([len(el) for el in oneFolder])
+    nHeight = len(oneFolder) 
+    nW = w // nWidth 
+    #nH = h // nHeight 
+    
+    hc = 0
+    spacer = 0
+    for c in range(0, len(folderLst)+1, nW):
+        lineFolders = folderLst[c:c+nW]
+        fLines = []
+        for i in range(nHeight):
+            fLines.append('')
+            for f in lineFolders:
+                fLines[i] += (f[i]) 
+                
+
+        for i, line in enumerate(fLines):
+            out = ('{:^' + str(w) + '}').format(line)
+            screen.addstr(hc*nHeight + i + spacer, 0, out)
+
+        spacer += 1
+        hc += 1
+
+
 class Element:
-    def __init__(self, name, x, y, width, height):
+    def __init__(self, name):
         self.name = name
-        self.x = x
-        self.y = y
-        fieldX = { i for i in range(x, x+width+1)}
-        fieldY = { j for j in range(y, y+height+1)}
-        self.field = (fieldX, fieldY)
+        self.width = 22
+        self.height = 10 
+        self.x = 0
+        self.y = 0
 
 class Controller:
-    c = 0
-    elements = []
-    dir = ""
-
     def __init__(self):
-        pass
+        self.curses, self.screen = init()
+        self.height, self.width = self.screen.getmaxyx()
+        self.c = 0
+        self.elements = []
+        self.dir = "" 
 
     def add(self, name):
-        e = Element(name, 0, self.c, len(name), 0)
+        e = Element(name)
         self.elements.append(e)
         print(name)
-        screen.addstr(self.c, 0, name)
         self.c += 1
+
+    def collision(self):
+        space = 0
+        oneEl = self.elements[0]
+        sideSpace = (self.width % oneEl.width)//2
+        nH = self.height // oneEl.height
+        nW = self.width // oneEl.width
+        
+        h = 0
+        for c in range(0, len(self.elements), nW):
+            line = self.elements[c:c+nW]
+            
+            for w, el in enumerate(line):
+                el.x = sideSpace + w*el.width
+                el.y = h*el.height + space
+
+            space += 1
+            h += 1
+                
+
+        for el in self.elements:
+            fieldX = { i for i in range(el.x, el.x+el.width+1)}
+            fieldY = { j for j in range(el.y, el.y+el.height+1)}
+            el.field = (fieldX, fieldY)
+
+
+    
+    def print(self):
+        createGrid( [folder(el.name) for el in self.elements], self.screen)
     
     def check(self, x, y):
         for el in self.elements:
@@ -49,34 +123,38 @@ def cmd(arg):
     return ss 
 
 def show(c):
-    screen.clear()
+    c.screen.clear()
     if c.dir != "":
         os.chdir(c.dir)
     folders = cmd(["ls", "-a"])
 
     for el in folders:
         c.add(el)
-
-    screen.refresh()
+        c.print()
+        c.collision()
+    c.screen.refresh()
     whait(c)
 
 
 def whait(c):
     while True:
-        event = screen.getch() 
+        event = c.screen.getch() 
         if event == ord("q"): break
-        if event == curses.KEY_MOUSE:
-            _, mx, my, _, _ = curses.getmouse()
-            y, x = screen.getyx()
+        if event == c.curses.KEY_MOUSE:
+            _, mx, my, _, _ = c.curses.getmouse()
             el = c.check(mx, my)
             if el != None and os.path.isdir(el.name):
                c.dir = el.name
                c.clear()
                show(c)
-            screen.refresh()
+            c.screen.refresh()
+    exit(c.curses)
 
-    curses.endwin()
-
-c = Controller()
-show(c)
-print("Window ended.")
+def main():
+    c = Controller()
+#    try:
+    show(c)
+#    except Exception as e:
+#        exit(c.curses)
+#        print(e)
+main()
